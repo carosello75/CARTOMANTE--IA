b95947ac21fb4c95c15b8416de781264dd30253d
carosello75 <143719714+carosello75@users.noreply.github.com>
2025-03-02 10:44:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TWILIO_ACCOUNT_SID = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-TWILIO_AUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-TWILIO_PHONE_NUMBER = '+17753837076'
-OPENAI_API_KEY = 'sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID', 'inserisci_il_tuo_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN', 'inserisci_il_tuo_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER', 'inserisci_il_tuo_NUMERO')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'inserisci_la_tua_API_KEY')
