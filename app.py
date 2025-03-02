8b905565506b6dfed3d5bb20f2d5cb6193dcc036
carosello75 <143719714+carosello75@users.noreply.github.com>
2025-03-02 10:08:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ import requests
 from google.cloud import texttospeech, speech_v1p1beta1 as speech
 from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, OPENAI_API_KEY
 
-# Log subito per vedere se parte
 print(">>> Cartomante AI sta partendo...")
 
-# Configura le credenziali Google
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials.json"
 
-# Configura OpenAI
 openai.api_key = OPENAI_API_KEY
 
 app = Flask(__name__)
