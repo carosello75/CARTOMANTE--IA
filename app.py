3d4159f5b1a2b86715df8926da2afd37d14bb7ca
carosello75 <143719714+carosello75@users.noreply.github.com>
2025-03-02 07:37:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def process_voice():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=False)
+
